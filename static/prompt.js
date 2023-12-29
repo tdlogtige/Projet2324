3792e3f8e1cc89202fa507d2dc9635758cd89ccd
maryamelyaagoubi <123840457+maryamelyaagoubi@users.noreply.github.com>
2023-12-29 10:15:47 +0100

    Update prompt.js feedback

--- static/prompt.js
+++ static/prompt.js
@@ -334,6 +334,31 @@ function displayQCM(data) {     //data doit être un dictionnaire
         update_student_feedback(id, "thumb-down")
     };
 
+
+    difficultySelect.addEventListener("change", function () {
+    const selectedDifficulty = difficultySelect.value;
+    if (selectedDifficulty) {
+        updateDifficultyLevel(id, selectedDifficulty);
+    }
+});
+
+
+    function updateStudentFeedback(id, feedback) {
+    fetch(`/update-student-feedback?id=${id}&feedback=${feedback}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+}
+
+    function updateDifficultyLevel(id, difficulty) {
+    fetch(`/update-difficulty-level?id=${id}&difficulty=${difficulty}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+}
+
+
+
     newQCMButton.addEventListener("click", handleNewQCMClick);
 
 }
